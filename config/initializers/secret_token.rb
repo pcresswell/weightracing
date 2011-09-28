059e84bc644b95f20da13c323350cb2add7ef437
Peter Cresswell <pcresswell@Peter-Cresswells-MacBook-Pro.local>
2011-09-28 16:04:55 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weightracing::Application.config.secret_token = '006279c76b2a14243a4f14fbf09e3381760c57e4d1775ab11cd0e5903562dcfaa59f27e5109df1d994bed3b0ca2d3d40c3cf6f0fff2bbe072511f940dae9ed64'
