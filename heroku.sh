922f94909e4569088b46515f92433441a82ab868
Peter Cresswell <pcresswell@Peter-Cresswells-MacBook-Pro.local>
2011-10-03 18:55:19 -0400

    update

--- heroku.sh
+++ heroku.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -ex
-source ~/.profile
-git rev-parse HEAD
-echo $GIT_COMMIT
-git push git@heroku.com:growing-sword-8460.git $GIT_COMMIT:master -v
+git push git@heroku.com:growing-sword-8460.git master -v
 
